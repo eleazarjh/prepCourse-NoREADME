5d73bc02d66d8de0809327a33dbc953d60a55d04
ele1227 <eleazarjh@gmail.com>
2023-02-23 15:29:05 -0500

    estas son las Variables de java scrit

---
+++ variables.js
@@ -0,0 +1,38 @@
+//variable:  una variable en programación se puede entender como un nombre que representa un valor y se utiliza de forma periódica para almacenar diversos tipos de datos, los cuales se utilizarán en las operaciones posteriores de codificación.//
+var nombre = "Eleazar Heredia";
+var edad = 50
+
+//declarar una Variable
+var edad;
+//Inicializada
+edad = 50 
+
+//declarar una variable
+var edad;
+//para inicalizar una variable ago lo siguiente 
+edad = 50
+
+//variables complejas 
+
+//variables tipo Objeto
+
+var persona = {
+    nombre: "Eleazar",
+    apellido: "Heredia",
+    edad: 50
+};
+
+//variable de tipo Array
+//array 
+var amigos = ["paulito, Eleazar, Jose, Julio"]
+var edades = [52, 20, 25, 34];
+
+
+var amigos = [
+    {nombre: "paulito", edad: 52},
+    {nombre: "eleazar", edad: 20},
+    {nombre: "jose", edad: 25},
+    {nombre: "julio", edad: 34},
+
+];
+
